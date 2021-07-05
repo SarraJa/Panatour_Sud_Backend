ccf66d5d4b36c53036e4b0ad0603047e9558764c
Sarra Jaziri <sarra.jaziri@etudiant-isi.utm.tn>
2021-07-05 10:07:08 +0100

    First commit

---
+++ src/Controller/Admin/ServiceDesMonumentsCrudController.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+use App\Entity\ServiceDesMonuments;
+use App\Form\ImageFormType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+
+class ServiceDesMonumentsCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return ServiceDesMonuments::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('Id')->onlyOnIndex(),
+            TextField::new('libele'),
+            TextField::new('type'),
+            TextField::new('adresse'),
+            TextField::new('description'),
+            MoneyField::new('prix')->setCurrency('TND'),
+            DateTimeField::new('CreatedAt'),
+            //TextField::new('imageFile',"Image  ")->setFormType(VichImageType::class)->onlyWhenCreating(),
+            CollectionField::new('images')
+                ->setEntryType(ImageFormType::class)
+                ->setFormTypeOption('by_reference',false)
+                ->onlyOnForms(),
+
+
+            CollectionField::new('images')->onlyOnDetail()->setTemplatePath('images.html.twig'),
+            //ImageField::new('file')->setBasePath('/uploads/imageservices/')->onlyOnIndex()
+        ];
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('adresse')
+            ->add('type')
+
+            ;
+    }
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+}
