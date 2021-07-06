81d289fb03c273b1cd2e51ee0b12d7eb2236cb1d
Sarra Jaziri <sarra.jaziri@etudiant-isi.utm.tn>
2021-07-06 08:41:31 +0100

    First commit

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -3,6 +3,9 @@
 namespace App\Security;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,23 +25,26 @@ use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 
 
-
-class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
+class AppCustomAuthenticator extends AbstractLoginFormAuthenticator implements PasswordAuthenticatedInterface
 {
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
     private $csrfTokenManager;
-
-
+    private $entityManager;
+    private $passwordEncoder;
     private $urlGenerator;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator , CsrfTokenManagerInterface $csrfTokenManager)
+    public function __construct(EntityManagerInterface $entityManager,UrlGeneratorInterface $urlGenerator , CsrfTokenManagerInterface $csrfTokenManager,UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->urlGenerator = $urlGenerator;
         $this->csrfTokenManager = $csrfTokenManager;
+        $this->entityManager = $entityManager;
+        $this->passwordEncoder = $passwordEncoder;
+
     }
 
     public function authenticate(Request $request): PassportInterface
@@ -55,7 +61,17 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
             ]
         );
     }
-
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function getPassword($credentials): ?string
+    {
+        return $credentials['password'];
+    }
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+    }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
@@ -79,7 +95,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         return $credentials['id'];
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    /*public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
 
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
@@ -87,14 +103,18 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 
              }
 
-       // return new RedirectResponse($this->urlGenerator->generate(
-        //    'user_profile',
-         //   ['id' => $token->getUser()]
-       // ));
-
 
         // For example:
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    }*/
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
